31ec141f8de8571eed6ada0772b808a255891a2e
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-09-06 20:05:13 -0600

    Add files via upload

---
+++ semana6-ejercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿//ejercicio1 melvin perez ramirez
+using System.Linq;
+
+int[] scores = { 97, 92, 81, 60 };
+
+//score > 80 //filtros
+var query = from score in scores
+            where score > 80
+            select score;
+
+foreach (var result in query)
+{
+    Console.WriteLine(result);
+}
---
+++ semana6-ejercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿//ejercicio2 melvin perez ramirez
+using System.Linq;
+
+// Fuente de datos
+int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+// Filtro: score > 80
+var query = from score in scores
+            where score > 80
+            orderby score descending
+            select score;
+
+// ordenados de manera descendente
+foreach (var result in query)
+{
+    Console.WriteLine(result);
+}
